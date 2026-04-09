adc90b3ecc3c74c3d9a7190d5233a3f52d6bed13
yunseok <dbstjr0219@gmail.com>
2026-04-09 15:49:55 +0900

    SendMail

--- backend/app/scheuler.py
+++ backend/app/scheuler.py
@@ -4,8 +4,12 @@ import schedule
 import time
 from email_service import send_email
 
+def get_emails_from_db():
+    # TODO: 나중에 DB 연결
+    return ["dbstjr0219@gmail.com"]
+
 def job():
-    emails = ["test@gmail.com"]  # 나중에 DB로 교체
+    emails = get_emails_from_db()
     content = "오늘 공지 요약입니다"
 
     for e in emails:
